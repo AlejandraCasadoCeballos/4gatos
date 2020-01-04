9a4cda895af1efa3b05857c96023a93ed30216f8
alejandracc <alejandracasado97@gmail.com>
2020-01-04 18:27:20 +0100

    interfaz de la escena de introducir nombre cambiada

--- F_3_GRUPO_G/4CatsAPI_REST/target/classes/static/Scene12.js
+++ F_3_GRUPO_G/4CatsAPI_REST/target/classes/static/Scene12.js
@@ -8,11 +8,9 @@ class Scene12 extends Phaser.Scene{
     }
  preload ()
  {
-	 this.load.image('introducir nombre','/Recursos/Interfaz/introducir nombre.jpg');// cargamos la imagen de introducir nombre
-	 this.load.image('introducir nombre pulsado','/Recursos/Interfaz/introducir nombre pulsado.jpg');// cargamos la imagen de los introducir nombre pulsado
+	 this.load.image('introducirNombre','/Recursos/Interfaz/introducirNombre.jpg');// cargamos la imagen de introducir nombre
+	 this.load.image('introducirNombreP','/Recursos/Interfaz/introducirNombreAtrasPulsado.jpg');// cargamos la imagen de los introducir nombre pulsado
      this.load.html('nameform','nameform.html');
-     this.load.image('introducir nombre atras','/Recursos/Interfaz/introducir nombre atras pulsado.jpg');// cargamos la imagen de los introducir nombre pulsado
-        
  }
 
  create ()
@@ -24,7 +22,7 @@ class Scene12 extends Phaser.Scene{
 	 musica2.resume();
 	 const botonAtras = this.add.text(90, 540,"Atras",{font:"30px Courier", fill:"Red"}).setOrigin(0.5,0.5);
 	 
-	 this.add.image(400, 300,'introducir nombre');
+	 this.add.image(400, 300,'introducirNombre');
    
 	 this.input.on('dragstart', function (pointer, gameObject) {
 		 this.bringToTop(gameObject);
@@ -35,7 +33,7 @@ class Scene12 extends Phaser.Scene{
 		 gameObject.y = dragY;
 	 });
  
-	 var element=  this.add.dom(400,300, 'div',  'font: 30px Agency FB bold','phaser').createFromCache('nameform');
+	 var element=  this.add.dom(400,320, 'div',  'font: 30px Agency FB bold','phaser').createFromCache('nameform');
 	 element.addListener("click");
   	 element.on("click", function (event) {
   		if (event.target.name === 'guardar')
@@ -48,8 +46,8 @@ class Scene12 extends Phaser.Scene{
 	 
 	 botonAtras.setInteractive();
 	 botonAtras.on('pointerdown', () => { DELETEservidor(jugador);musica2.stop();this.scene.start("Menu");});
-     botonAtras.on('pointerover', () => {this.add.image(400, 300,'introducir nombre atras'); });
-     botonAtras.on('pointerout', () => {this.add.image(400, 300,'introducir nombre'); });
+     botonAtras.on('pointerover', () => {this.add.image(400, 300,'introducirNombreP'); });
+     botonAtras.on('pointerout', () => {this.add.image(400, 300,'introducirNombre'); });
      
 }
     update(){
